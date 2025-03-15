fe36e9b92404ac9118626f5d3751c3115b3ea401
Jonas Pleyer <jonas.dev@pleyer.org>
2025-03-15 17:20:52 +0100

    move code to main file

--- src/disjoint.rs
+++ src/disjoint.rs
@@ -4,36 +4,7 @@ use approx_derive::RelativeEq;
 use num_traits::AsPrimitive;
 use simba::scalar::RealField;
 
-/// Generalized cuboid in `D` dimensions
-///
-/// ```
-/// use spatial_decomposition::*;
-/// use approx::assert_abs_diff_eq;
-///
-/// let c1 = Cuboid {
-///     min: [1.; 4],
-///     max: [15., 15., 10., 10.],
-/// };
-/// let c2 = Cuboid {
-///     min: [1.1; 4],
-///     max: [14.9, 14.99, 10.02, 9.97],
-/// };
-///
-/// assert_abs_diff_eq!(c1, c2, epsilon = 0.11);
-/// ```
-#[derive(Clone, Debug, PartialEq, RelativeEq)]
-#[approx(epsilon_type = F)]
-pub struct Cuboid<F, const D: usize> {
-    /// Lower bounds of cuboid
-    #[approx(into_iter)]
-    pub min: [F; D],
-    /// Upper bounds of cuboid
-    #[approx(into_iter)]
-    pub max: [F; D],
-}
-
-/// `2D` variant of the [Cuboid]
-pub type Rectangle<F> = Cuboid<F, 2>;
+use crate::Rectangle;
 
 #[allow(non_camel_case_types)]
 enum Decomposition<F> {
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,40 @@
-//! Description
+//! This crate contains algorithms to decompose spatial sets into smaller subdomains.
+//! To this date, only `1D` and `2D` algorithms are implemented.
+//!
+//! ## 2D
 
+use approx_derive::RelativeEq;
+
+/// Generalized cuboid in `D` dimensions
+///
+/// ```
+/// use spatial_decomposition::*;
+/// use approx::assert_abs_diff_eq;
+///
+/// let c1 = Cuboid {
+///     min: [1.; 4],
+///     max: [15., 15., 10., 10.],
+/// };
+/// let c2 = Cuboid {
+///     min: [1.1; 4],
+///     max: [14.9, 14.99, 10.02, 9.97],
+/// };
+///
+/// assert_abs_diff_eq!(c1, c2, epsilon = 0.11);
+/// ```
+#[derive(Clone, Debug, PartialEq, RelativeEq)]
+#[approx(epsilon_type = F)]
+pub struct Cuboid<F, const D: usize> {
+    /// Lower bounds of cuboid
+    #[approx(into_iter)]
+    pub min: [F; D],
+    /// Upper bounds of cuboid
+    #[approx(into_iter)]
+    pub max: [F; D],
+}
+
+/// `2D` variant of the [Cuboid]
+pub type Rectangle<F> = Cuboid<F, 2>;
 mod disjoint;
 
 pub use disjoint::*;
