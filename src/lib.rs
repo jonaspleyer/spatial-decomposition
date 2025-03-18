8fee18b6a0b58182721d99b179e7fbda6945d01b
Jonas Pleyer <jonas.dev@pleyer.org>
2025-03-18 16:45:16 +0100

    bump to 0.2.1
    - also extend table with methods

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,11 @@
 //! To this date, only `1D` and `2D` algorithms are implemented.
 //!
 //! ## Overview
-//! | Dim | Domain | Method | Paper |
-//! | --- | --- | --- | --- |
-//! | `2D` | [Rectangle] | [kmr_decompose] | [Kong, Mount and Roscoe](https://scispace.com/pdf/the-decomposition-of-a-rectangle-into-rectangles-of-minimal-3whu99wjdy.pdf) |
+//! | Domain | Function | Reference |
+//! |:--- | --- |:--- |
+//! | [Rectangle] | [kmr_decompose] | [Kong, Mount and Roscoe](https://scispace.com/pdf/the-decomposition-of-a-rectangle-into-rectangles-of-minimal-3whu99wjdy.pdf) |
+//! | [Rectangle] | [kmr_digitize_1] | [Kong, Mount and Roscoe](https://scispace.com/pdf/the-decomposition-of-a-rectangle-into-rectangles-of-minimal-3whu99wjdy.pdf) |
+//! | [Rectangle] | [kmr_digitize_1_single] | [Kong, Mount and Roscoe](https://scispace.com/pdf/the-decomposition-of-a-rectangle-into-rectangles-of-minimal-3whu99wjdy.pdf) |
 
 use approx_derive::RelativeEq;
 
